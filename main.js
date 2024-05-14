53a4b7672bcd3c398a96fb1bac893cfd52d353f8
Coco-Bd <146067674+Coco-Bd@users.noreply.github.com>
2024-05-14 14:16:58 +0200

    classname added

--- main.js
+++ main.js
@@ -1,48 +1,51 @@
-//Thisfunctioni scalledonlyafterthedatahasbeenfetched,andparsed. 
-const loadData = heroes =>{
-    console.log(heroes)
-    }
-
+//Thisfunctioni scalledonlyafterthedatahasbeenfetched,andparsed.
+const loadData = (heroes) => {
+  console.log(heroes);
+};
 
 function afficherInformations(page = 0) {
-    var infoCount = document.getElementById('infoCount').value;
-    fetch('https://raw.githack.com/akabab/superhero-api/0.2.0/api/all.json')
-        .then(response => response.json())
-        .then(data => {
-            var infoContainer = document.getElementById('infoContainer');
-            infoContainer.innerHTML = '';
-            data.forEach(info => {
-                if (info.id >= page*infoCount && info.id < (page+1)*infoCount) {
-                    var div = document.createElement('div');
-                    var p = document.createElement('p');
-                    var img = document.createElement('img');
+  var infoCount = document.getElementById("infoCount").value;
+  fetch("https://raw.githack.com/akabab/superhero-api/0.2.0/api/all.json")
+    .then((response) => response.json())
+    .then((data) => {
+      var infoContainer = document.getElementById("infoContainer");
+      infoContainer.innerHTML = "";
+      data.forEach((info) => {
+        if (info.id >= page * infoCount && info.id < (page + 1) * infoCount) {
+          var div = createGameElement("div", "info");
+          var p = createGameElement("p", "name");
+          var img = createGameElement("img", "image");
 
-                    p.textContent = info.name;
-                    img.src = info.images.xs;
+          p.textContent = info.name;
+          img.src = info.images.xs;
 
-                    div.appendChild(img);
-                    div.appendChild(p);
-                    infoContainer.appendChild(div);
-                }
-                
-            });
-            loadData(data);
-        }) 
-        .catch(error => console.error('Erreur:', error));
+          div.appendChild(img);
+          div.appendChild(p);
+          infoContainer.appendChild(div);
+        }
+      });
+      loadData(data);
+    })
+    .catch((error) => console.error("Erreur:", error));
 }
 
-
 var page = 0;
-afficherInformations(page)
+afficherInformations(page);
 function select_page(direction) {
-    if (direction === '-') {
-        if (page > 0) {
-            page--;
-            afficherInformations(page)
-        }
-    } else if (direction === '+') {
-        page++;
-        afficherInformations(page)
+  if (direction === "-") {
+    if (page > 0) {
+      page--;
+      afficherInformations(page);
     }
-    document.getElementById('page').textContent = page;
+  } else if (direction === "+") {
+    page++;
+    afficherInformations(page);
+  }
+  document.getElementById("page").textContent = page;
+}
+
+function createGameElement(element, className) {
+  const gameElement = document.createElement(element);
+  gameElement.className = className;
+  return gameElement;
 }
