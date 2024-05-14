f27edf2ca8de705b58ffbe9dc08a5b7b000a283e
rodove-tv <enzo.martinez.travail@gmail.com>
2024-05-14 14:04:43 +0200

    add page change and artist change depending on the page

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const loadData = heroes =>{
     }
 
 
-function afficherInformations() {
+function afficherInformations(page = 0) {
     var infoCount = document.getElementById('infoCount').value;
     fetch('https://raw.githack.com/akabab/superhero-api/0.2.0/api/all.json')
         .then(response => response.json())
@@ -12,11 +12,17 @@ function afficherInformations() {
             var infoContainer = document.getElementById('infoContainer');
             infoContainer.innerHTML = '';
             data.forEach(info => {
-                if (info.id <= infoCount) {
+                if (info.id >= page*infoCount && info.id < (page+1)*infoCount) {
+                    var div = document.createElement('div');
                     var p = document.createElement('p');
-                    p.textContent = info.id;
+                    var img = document.createElement('img');
+
                     p.textContent = info.name;
-                    infoContainer.appendChild(p);
+                    img.src = info.images.xs;
+
+                    div.appendChild(img);
+                    div.appendChild(p);
+                    infoContainer.appendChild(div);
                 }
                 
             });
@@ -25,16 +31,18 @@ function afficherInformations() {
         .catch(error => console.error('Erreur:', error));
 }
 
-function select_page() {
-    var page = document.getElementById('page').value;
-    var page_less = document.getElementById('page_less').value;
-    var page_more = document.getElementById('page_more').value;
 
-    if (page_less) {
+var page = 0;
+afficherInformations(page)
+function select_page(direction) {
+    if (direction === '-') {
         if (page > 0) {
             page--;
+            afficherInformations(page)
         }
-    } else if (page_more) {
+    } else if (direction === '+') {
         page++;
+        afficherInformations(page)
     }
+    document.getElementById('page').textContent = page;
 }
