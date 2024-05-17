85ce98484923e1bc852f9235c4d6d8020eacf8dc
rodove-tv <enzo.martinez.travail@gmail.com>
2024-05-17 17:06:52 +0200

    add server

---
+++ serveur.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(function(req, res, next) {
+  if (req.originalUrl.endsWith('.js')) {
+    res.type('application/javascript');
+  }
+  next();
+});
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 3032;
+
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port http://localhost:${PORT}`);
+});
